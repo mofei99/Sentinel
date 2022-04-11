a47e2dfef837819608c7b244bd65a264ea4773a9
mofei99 <mofei100@163.com>
2022-04-11 17:05:05 +0800

    RateLimiterController添加注释

--- sentinel-core/src/main/java/com/alibaba/csp/sentinel/slots/block/flow/controller/RateLimiterController.java
+++ sentinel-core/src/main/java/com/alibaba/csp/sentinel/slots/block/flow/controller/RateLimiterController.java
@@ -26,10 +26,11 @@ import com.alibaba.csp.sentinel.node.Node;
  * @author jialiang.linjl
  */
 public class RateLimiterController implements TrafficShapingController {
-
+    //队列最大等待时间
     private final int maxQueueingTimeMs;
+    //限流数
     private final double count;
-
+    //最近通过请求的时间
     private final AtomicLong latestPassedTime = new AtomicLong(-1);
 
     public RateLimiterController(int timeOut, double count) {
@@ -44,41 +45,57 @@ public class RateLimiterController implements TrafficShapingController {
 
     @Override
     public boolean canPass(Node node, int acquireCount, boolean prioritized) {
-        // Pass when acquire count is less or equal than 0.
+        // 请求令牌小于0，直接通过
         if (acquireCount <= 0) {
             return true;
         }
-        // Reject when count is less or equal than 0.
+        // count<=0直接通过
         // Otherwise,the costTime will be max of long and waitTime will overflow in some cases.
         if (count <= 0) {
             return false;
         }
 
         long currentTime = TimeUtil.currentTimeMillis();
-        // Calculate the interval between every two requests.
+        // 计算两个请求之间的间隔时间，单位:ms
+        //
+        /**
+         * 计算两个请求之间的间隔时间，单位:ms
+         * 假设限流为1000QPS/秒，则间隔时间为1ms
+         */
         long costTime = Math.round(1.0 * (acquireCount) / count * 1000);
 
-        // Expected pass time of this request.
+        //
+        /**
+         * 期望时间：上次请求通过时间+间隔时间
+         * 期望通过时间<=currentTime则直接放行
+         */
         long expectedTime = costTime + latestPassedTime.get();
 
         if (expectedTime <= currentTime) {
-            // Contention may exist here, but it's okay.
+            // 可能存在并发问题，为了性能，可以接受
+            // TODO: 2022/4/11  学习AtomicLong
             latestPassedTime.set(currentTime);
             return true;
         } else {
-            // Calculate the time to wait.
+            // 计算等待时间
             long waitTime = costTime + latestPassedTime.get() - TimeUtil.currentTimeMillis();
+            //如果大于最大排队时间，则直接拒绝
             if (waitTime > maxQueueingTimeMs) {
                 return false;
             } else {
+                //更新最近放行时间
                 long oldTime = latestPassedTime.addAndGet(costTime);
                 try {
                     waitTime = oldTime - TimeUtil.currentTimeMillis();
+                    //二次校验
                     if (waitTime > maxQueueingTimeMs) {
                         latestPassedTime.addAndGet(-costTime);
                         return false;
                     }
-                    // in race condition waitTime may <= 0
+                    /**
+                     * 等待一段时间再放行
+                     * 由于并发问题，此处waitTime可能小于等于0
+                     */
                     if (waitTime > 0) {
                         Thread.sleep(waitTime);
                     }
